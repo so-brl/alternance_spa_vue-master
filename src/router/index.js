60c0f03d638a6fb38e8aa63fce425df115f78a5b
so-brl <Ca167043Soso12@>
2021-02-17 10:33:59 +0100

    UPD [VUE] Migrate from Vue 2 to Vue 3 - Part 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import CitiesList from "../components/CitiesList";
-import CitiesMap from "../components/CitiesMap";
-import City from "../components/City";
-import EasterEgg from "../components/EasterEgg";
-
-Vue.use(VueRouter);
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes = [
-  { path: '/villes', component: CitiesList },
-  { path: '/carte', component: CitiesMap },
-  { path: '/ville', component: City },
-  { path: '/onenagros', component: EasterEgg}
-];
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
 
-export const router = new VueRouter({
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
-});
+})
+
+export default router
